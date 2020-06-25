859438c2a67a2a3165ba32984252d3619e97fdb2
jalaj lasod <jalajlasod26@gmail.com>
2020-06-25 14:53:50 +0530

    update

--- app.js
+++ app.js
@@ -75,7 +75,7 @@
       service.query=function () {
         return $http({
 
-          url:"http://davids-restaurant.herokuapp.com/menu_items.json"
+          url:"https://davids-restaurant.herokuapp.com/menu_items.json"
 
         }).then(function (response) {
 
